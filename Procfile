537f1a665c62e581a80f28625354624df5d4ef87
Your Name <you@example.com>
2024-10-16 13:36:33 +0300

    16.10.24 ПЗ11 Дубровский Подготовил проект для отправки на питонэнивер

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn webbook.wsgi --log-file –
--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,14 +26,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-ioc+_2mxk2-&=bb+jcd6*p=%d7(9kacsg*izaywsx^%d-p9=hw'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 
-'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get("SECRET_KEY")  
+
+if os.environ.get("DEBUG") == "False":
+    DEBUG = False
+else:
+    DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ["127.0.0.1", "loughtyy.pythonanywhere.com"]
 
 
 # Application definition
@@ -82,8 +92,15 @@ WSGI_APPLICATION = 'WebBooks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",  
+        "NAME": os.getenv("MYSQL_DBNAME"),  
+        "USER": os.getenv("MYSQL_USER"),  
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  
+        "HOST": os.getenv("MYSQL_HOST"),  
+        "OPTIONS": {  
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",  
+            "charset": "utf8mb4",
+        },  
     }
 }
 
@@ -126,6 +143,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = "media/"  
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
