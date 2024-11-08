0b0959204da8d2bf7c507a164912ee23a677d821
Your Name <you@example.com>
2024-11-08 09:04:06 +0300

    08.11.24 ЛЗ17 Журавлёв Научился пользоваться моделями данных.

--- catalog/models.py
+++ catalog/models.py
@@ -105,6 +105,22 @@ class BookInstance(models.Model):
  verbose_name="Заказчик",
  help_text='Выберите заказчика книги')
 
+class Person(models.Model):
+ name = models.CharField(max_length=20)
+ age = models.IntegerField()
+ object_person = models.Manager()
+ DoesNotExist = models.Manager
+
+class Company(models.Model):
+ name = models.CharField(max_length=30)
+
+class Product(models.Model):
+ company = models.ForeignKey(Company, on_delete=models.CASCADE)
+ name = models.CharField(max_length=30)
+ price = models.IntegerField()
+
+
+
  
 
 
