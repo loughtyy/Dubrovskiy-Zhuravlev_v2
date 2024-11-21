29f9e8c90215d683553c2601adc033841fa9d169
Your Name <you@example.com>
2024-11-21 12:50:19 +0300

    21.11.24 ЛЗ23 Журавлёв Научился создавать Django API используя Django Rest Framework.

--- test_django/article/urls.py
+++ test_django/article/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ArticleView
+from .views import ArticleView, SingleArticleView
 app_name = "articles"
 
 urlpatterns = [
  path('articles/', ArticleView.as_view()),
- path('articles/<int:pk>', ArticleView.as_view()),
+ path('articles/<int:pk>', SingleArticleView.as_view()),
 ]
